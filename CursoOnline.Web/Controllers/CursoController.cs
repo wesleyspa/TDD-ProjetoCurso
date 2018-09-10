953d0cfd054f117be62ff5678fb9c960f33676ac
Wesley Alves <wesleyspa@yahoo.com.br>
2018-09-09 23:25:44 -0300

    Projeto web com controllers, model e view, Utils de paginacao e Library futura de IoC

---
+++ CursoOnline.Web/Controllers/CursoController.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+using CursoOnline.Dominio.Cursos;
+using CursoOnline.Web.Util;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoOnline.Web.Controllers
+{
+    [Route("Curso")]
+    public class CursoController : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var cursos = new List<CursoParaListagemDto>();
+            return View("Index", PaginatedList<CursoParaListagemDto>.Create(cursos, Request));
+        }
+
+        public IActionResult Novo()
+        {
+            return View("NovoOuEditar", new CursoDto());
+        }
+
+        [HttpPost]
+        public IActionResult Salvar(CursoDto model)
+        {
+            return Ok();
+        }
+    }
+}
